0d05b54e62424a301ce91ab4cd6fc094969b686c
Léandro DA SILVA <leothefrench@gmail.com>
2024-09-09 10:25:34 +0200

    implémentation Top Products

--- components/dashboard/TopProducts.tsx
+++ components/dashboard/TopProducts.tsx
@@ -1,11 +1,56 @@
+'use client'
+
 import { AnalyticsCard } from "./AnalyticsCard"
+import { ColumnDef } from "@tanstack/react-table"
+import Image from "next/image"
+import { DataTable } from "../ui/data-table"
+import { ProductsDummyData } from "@/constants/data"
+
+export type TopProducts = {
+  id: number;
+  name: string;
+  revenue: number;
+  price: number;
+  image: string;
+}
+
+export const TopProductsColumns: ColumnDef<TopProducts>[] = [
+  {
+    accessorKey: 'name',
+    header: 'Name',
+  },
+  {
+    accessorKey: 'revenue',
+    header: 'Revenue',
+  },
+  {
+    accessorKey: 'image',
+    header: 'Image',
+    cell: ({row}) => {
+      const imageUrl = row.getValue('image') as string
+
+      return (
+        <Image
+          src={imageUrl}
+          width={50}
+          height={50}
+          alt={`Product Image`}
+          className="border-2 border-primary"
+        />
+      )
+    }
+  },
+];
 
 export const TopProducts = () => {
+  const TopProducts = ProductsDummyData.sort((a, b) => b.revenue - a.revenue).slice(0,4)
+
   return (
-    <AnalyticsCard title="Top Products" subTitle="Showing Most Top Products">
-        <div>
-            TOP PRODUCTS
-        </div>
+    <AnalyticsCard title="Top Products" subTitle="Showing Most Sold Products">
+        <DataTable 
+          columns={TopProductsColumns}
+          data={TopProducts}
+        />
     </AnalyticsCard>
   )
 }
